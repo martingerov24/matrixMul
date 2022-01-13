d3d0eabd5e684e31c926980845a8ddbe3438817e
Martin Gerov <mgerov@software-supreme.com>
2022-01-13 19:44:29 +0200

    learned the big 5

--- external/matrix.h
+++ external/matrix.h
@@ -4,29 +4,81 @@
 class Matrix
 {
 public:
-	Matrix(std::vector<int32_t> && matrix, uint8_t x, uint8_t y) noexcept
-		:m_matrix(std::move(matrix)), m_x(x), m_y(y)
+	Matrix(std::vector<int32_t>& matrix, uint8_t x, uint8_t y, int id) noexcept
+		:m_matrix(matrix), m_x(x), m_y(y), id(id)
 	{
+		printf("constructor\n");
+	}
+	~Matrix() 
+	{
+		printf("destructor\n");
+	}
+	Matrix(const Matrix && other) noexcept
+		: m_matrix(std::move(other.m_matrix))
+		, m_x(other.m_x)
+		, m_y(other.m_y)
+		,id(other.id+1)
+	{
+		printf("move\n");
+	}
+	
+	Matrix(const Matrix& other) noexcept
+		: m_matrix(other.m_matrix)
+		, m_x(other.m_x)
+		, m_y(other.m_y)
+
+	{
+		printf("BOOM");
+	}
+	
+	Matrix& operator=(Matrix& other) 
+	{
+		printf("assigment\n");
+		return *this;
 	}
 
-	Matrix& operator*(Matrix& other)
+	Matrix& operator=( Matrix && other) noexcept
+	{
+		printf("Move Ass");
+		if (other.m_matrix == m_matrix)
+		{
+			return *this;
+		}
+		m_matrix = std::move(other.m_matrix);
+		return *this;
+	}
+
+	const Matrix operator*(const Matrix& other) noexcept
 	{
 		if (m_matrix == other.m_matrix
 			|| m_y != other.m_x
 			|| m_x != other.m_y)
 		{return *this;}
-		Matrix result(std::vector<int32_t> {0}, m_x, other.m_y);
 
-		for (int i = 0; i < m_x*m_y; ++i)
+		Matrix result(std::vector<int32_t>(0), m_x, other.m_y, 30);
+		result.m_matrix.resize(other.m_y * m_x);
+		
+		int32_t res = 0;
+		uint8_t count = 0;
+		uint8_t otherCount = 0;
+		for (int i = 0; i < result.m_matrix.size(); ++i)
 		{
-			for (int j = 0; j < other.m_x; ++j)
-			{
-				
-			}
+			if (i % result.m_x == 0 && i != 0)
+				{ count++; otherCount = 0; }
+			uint8_t cur = count * m_x;
+			res += m_matrix[cur] *	   other.m_matrix[otherCount];
+			res += m_matrix[cur + 1] * other.m_matrix[otherCount + m_x];
+			res += m_matrix[cur + 2] * other.m_matrix[otherCount + m_x * 2];
+			result.m_matrix[i] = res;
+			otherCount++;
+			res = 0;
 		}
+
+		return std::move(result);
 	}
-private:
+
 	uint8_t m_x;
 	uint8_t m_y;
 	std::vector<int32_t> m_matrix;
+	int id = 0;
 };
--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
-#include <iostream>
 #include "external/matrix.h"
+
+
+Matrix createTmp2() noexcept {
+	return Matrix(std::vector<int32_t>(0), 3, 3, 40);
+}
+
+Matrix createTmp() noexcept {
+	return createTmp2();
+}
+
 int main()
 {
-	Matrix matrix(std::vector<int32_t> {1, 2, 3, 4, 5, 4, 3, 2, 1}, 3, 3); // this is temporary vector for move ctor
-	Matrix reuslt = matrix.operator*( Matrix
-											(std::vector<int32_t>{1, 0, 0, 0, 1, 0, 0, 0, 1}, 3, 3));
+	Matrix matrix(std::vector<int32_t> {1, 2, 3, 4, 5, 4, 3, 2, 1}, 3, 3, 10); // this is temporary vector for move ctor
+	Matrix mat2(std::vector<int32_t>{1, 0, 0, 0, 1, 0, 0, 0, 1}, 3, 3, 20);
+	
+	//mat2 = matrix * mat2;
+	//matrix = std::move(mat2);
+	
+	//Matrix equ = matrix * mat2;
+	const Matrix result(matrix * mat2);
+	//const Matrix result(createTmp());
+
+	for (int i = 0; i < result.m_x * result.m_y; i++)
+	{
+		printf("%d ", result.m_matrix[i]);
+	}
 	return 0;
 }
